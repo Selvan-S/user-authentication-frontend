6e48668fae3802264bbfc9fd9bc8fc4b79040846
Selvan S <87435305+Selvan-S@users.noreply.github.com>
2023-11-10 12:32:22 +0530

    Revert changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react';
-import './App.css'
-import SignUp from './pages/SignUp';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import { Route, Routes, HashRouter } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter>
+    <>
       <Routes>
         <Route path="/" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
-    </HashRouter>
+    </>
   );
 }
 
-export default App
+export default App;
