197892bcefd6d82e79e2f4178eba17692398ed73
Selvan S <87435305+Selvan-S@users.noreply.github.com>
2023-12-03 00:17:41 +0530

    remove log and fixed name in home page

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div>
       <Routes>
         <Route path="/" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
